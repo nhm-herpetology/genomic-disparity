a15118b8340b9a38ec2dd6f1c0af70ff150f6051
Streicher Lab <35033128+nhm-herpetology@users.noreply.github.com>
2024-06-08 18:48:17 +0100

    Update chromosome_download.sh

--- Mammal-uces/chromosome_download.sh
+++ Mammal-uces/chromosome_download.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Welcome to Genomic Disparity Analysis... please ensure that your config file is formatted correctly"
+
+echo "Extracting information from config file..."
+
 awk '{print "mkdir "$1}' chromosome_download.config | sh
 
 awk '!/^$/{print  > "Taxon_"NR".txt" }' chromosome_download.config
@@ -10,9 +14,12 @@ for f in *.txt; do
   mv "$f" "$dir"
 done
 
-# 4. Execute download for each taxon, renaming the chromosomes with species names
+echo "Starting chromosome downloads..."
 
-cd Taxon_1
+for dir in */; do
+	(
+	echo "Fetching $dir chromosomes"
+	cd $dir
 
 awk -v n=3 '{ for (i=n; i<=NF; i++) printf "%s%s", $i, (i<NF ? OFS : ORS)}' Taxon_*.txt > search.txt
 
@@ -23,10 +30,14 @@ do
    esearch -db nucleotide -query $i | efetch -format fasta > "$i".fasta
 done
 
-# 5. Rename chromosomes with species name and remove quotes
+echo "Finished chromosome downloads... renaming files"
 
 VAR1="$(awk '{print $2}' Taxon_*.txt)"
 
 for f in *.fasta ; do mv -- "$f" "${VAR1}_$f" ; done
 
 find . -name '*\"*' | while read f; do mv "$f" "${f//\"/}"; done
+
+)done
+
+echo "Chromosomes prepped for analysis... script finished."
