c0725cf3844c4f5be60045b9115b376ab6e3fdbf
Streicher Lab <35033128+nhm-herpetology@users.noreply.github.com>
2024-06-08 18:10:32 +0100

    Create chromosome_download.sh

---
+++ Mammal-uces/chromosome_download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+awk '{print "mkdir "$1}' chromosome_download.config | sh
+
+awk '!/^$/{print  > "Taxon_"NR".txt" }' chromosome_download.config
+
+for f in *.txt; do
+  [[ -f "$f" ]] || continue
+  dir="${f%.*}"
+  mv "$f" "$dir"
+done
+
+# 4. Execute download for each taxon, renaming the chromosomes with species names
+
+cd Taxon_1
+
+awk -v n=3 '{ for (i=n; i<=NF; i++) printf "%s%s", $i, (i<NF ? OFS : ORS)}' Taxon_*.txt > search.txt
+
+declare -a chromosomes="$(head -n 1 search.txt)" 
+
+for i in ${chromosomes[@]}
+do
+   esearch -db nucleotide -query $i | efetch -format fasta > "$i".fasta
+done
