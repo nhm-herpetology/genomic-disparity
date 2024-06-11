54bbbf9701d0788b8debe64250b8d515e7ccbd54
Streicher Lab <35033128+nhm-herpetology@users.noreply.github.com>
2024-06-11 19:16:38 +0100

    Update chromosome_retriever.sh

--- Mammal-uces/chromosome_retriever.sh
+++ Mammal-uces/chromosome_retriever.sh
@@ -6,5 +6,21 @@ sed 's/,/ /g' present_landmarks.csv > resultfile
 
 head -n1 -q resultfile > new-file
 
-awk -v n=3 '{ for (i=n; i<=NF; i++) printf "%s%s", $i, (i<NF ? OFS : ORS)}' new-file > chromosomes-to-find.txt
+awk -v n=3 '{ for (i=n; i<=NF; i++) printf "%s%s", $i, (i<NF ? OFS : ORS)}' new-file > almost-there.txt
 
+sed 's/"//g' almost-there.txt > chromosomes-to-find.txt
+
+rm almost-there.txt
+rm new-file
+rm resultfile
+
+mkdir chromosome_set
+
+declare -a chromosomes="$(head -n 1 chromosomes-to-find.txt)" 
+
+for i in ${chromosomes[@]}
+do
+  
+find ./Taxon_** -name "mapped_$i.fasta.tsv" -exec cp "{}" ./chromosome_set \;
+
+done
